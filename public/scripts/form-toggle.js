698eb4f9e4ecb89936a28a8cd48cfd0a85fe11a0
Selin Huang <selin.hyx@gmail.com>
2019-07-17 23:42:04 +0000

    added second button to scroll back to top of page

--- public/scripts/form-toggle.js
+++ public/scripts/form-toggle.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
+
   $('.arrow-img').click(function() {
     $('.new-tweet').slideToggle();
   });
 
   $('.alert').hide();
+
+
+
+  /**************** Second Toggle: To Top Button ***************/
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 500) {
+        $('.to-top').fadeIn();
+    } else {
+        $('.to-top').hide();
+    }
+  }).trigger('scroll');
+
+  $('.to-top').click(function() {
+    $("html, body").animate({scrollTop: 0 }, 1000);
+  });
+
 });
+
+
 
